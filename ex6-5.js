7d0712d135f6c803dd051f04432bdba89572cafa
zyoshiki1110 <zm23441@josai.ac.jp>
2025-10-28 10:04:21 +0900

    Fix variable declaration and function syntax

--- ex6-5.js
+++ ex6-5.js
@@ -1,3 +1,3 @@
-var who65 = 'worid'; function sayhello65(){alert('Hello, ' + who65 + '!');}
+var who65='worid'; function sayhello65(){alert('Hello, ' + who65 + '!');}
 function taro65(){who65='太郎';}
 function hanako65(){who65='花子';}
