77ce559b8ee563ed3c9f3a4aa5d4506ec31deae0
zyoshiki1110 <zm23441@josai.ac.jp>
2025-10-28 10:21:16 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -1,3 +1,3 @@
-var who65='worid'; function sayhello65(){alert('Hello, ' + 'world' + 'が来た');}
-function taro65(){who65='太郎';}
-function hanako65(){who65='花子';}
+var who66='worid'; function sayhello66(){alert('Hello, ' + 'world' + 'が来た');}
+function taro66(){who66='太郎';}
+function hanako66(){who66='花子';}
