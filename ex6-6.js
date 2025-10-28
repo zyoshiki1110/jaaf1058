b6a88e73e96190b5099af58afb62a54f0550b39a
zyoshiki1110 <zm23441@josai.ac.jp>
2025-10-28 10:43:23 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -3,5 +3,5 @@ function taro66(){who66='太郎';}
 function hanako66(){who66='花子';}
 function come66(){alert(who66 + "が来た!");}
 function tarocome66(){taro66(); come66();}
-function hanakocome66(){hanako66(); come();}
+function hanakocome66(){hanako66(); come66();}
 
