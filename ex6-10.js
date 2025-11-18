7940e45161702559b8090fd4387d24627144e3b9
zyoshiki1110 <zm23441@josai.ac.jp>
2025-11-18 10:16:34 +0900

    Fix function name and container check logic

--- ex6-10.js
+++ ex6-10.js
@@ -1,7 +1,7 @@
-function changeimage(){const container = document.getElementBYId('imageContainer');}
+function gazo610(){const container = document.getElementById('gazo610');}
 const img1 = 'kaiei_small.jpg';
 const img2 = '20240721.jpg';
-if (container) return;
+if (!container) return;
 if (container.innerHTML.includes(img1))
 {container.innerHTML = '<img src="${img2}" alt="変更後の画像">';}
 else{container.innerHTML = '<img src="${img1}" alt="最初の画像">';}
