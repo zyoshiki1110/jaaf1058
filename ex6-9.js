832af474b29fbafc88a2ab5c00b5ebd5eb2945d8
zyoshiki1110 <zm23441@josai.ac.jp>
2025-11-18 09:48:30 +0900

    Update ex6-9.js

--- ex6-9.js
+++ ex6-9.js
@@ -1,3 +1,3 @@
-function sayhello69(){alert('Hello,' + document.getElementByld('who69').innerHTML + '!');}
-function taro69(){document.getElementByld('who69').innerHTML='太郎';}
-function hanako69(){document.getElementByld('who69').innerHTML='花子';}
+function sayhello69(){alert('Hello,' + document.getElementById('who69').innerHTML + '!');}
+function taro69(){document.getElementById('who69').innerHTML='太郎';}
+function hanako69(){document.getElementById('who69').innerHTML='花子';}
