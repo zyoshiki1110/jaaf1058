6906bc7d21f99fa8d5140949aa9057c27f66cfe3
zyoshiki1110 <zm23441@josai.ac.jp>
2025-11-18 09:30:15 +0900

    Create ex6-9.js

---
+++ ex6-9.js
@@ -0,0 +1,3 @@
+function sayhello69(){alert('Hello,'+ document.getElementByld('who69').innerHTML+'!');}
+function taro69(){document.getElementByld('who69').innerHTML='太郎';}
+function hanako69(){document.getElementByld('who69').innerHTML='花子';}
