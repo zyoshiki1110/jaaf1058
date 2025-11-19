133ab27beb80e9f6f20a861916dd21b54c263a84
zyoshiki1110 <zm23441@josai.ac.jp>
2025-11-19 10:51:32 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,2 +1,2 @@
-function gazo610(){document.getElementById("maypic").innerHTML = "<imag src='keiei_small.jpg' width='300'>";}
-function changeimage2(){document.getElementById("maypic").innerHTML = "<imag src='20240712.jpg' width='300'>";}
+function gazo610(){document.getElementById("who610").innerHTML = "<imag src='keiei_small.jpg' width='300'>";}
+function gazo610(){document.getElementById("who610").innerHTML = "<imag src='20240712.jpg' width='300'>";}
