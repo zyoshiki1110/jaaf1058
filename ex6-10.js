a449c0570e7a3682c78f3cdafec170a181b0880d
zyoshiki1110 <zm23441@josai.ac.jp>
2025-11-25 22:59:55 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,9 +1,4 @@
-function gazo610(){const IMAGE_1 = '20240712.jpg'; const IMAGE_2 = 'keiei_small.jpg';
-const imgElement = document.getElementById('who610');
-const statusElement = document.getElementById('who611');
-if (imgElement.src.endsWith(IMGE_1)){imgElement.src. = IMAGE_2; statusElement.innerHTML = '画像2(' + IMAGE_2 + ')';}
-else{imgElement.src =IMAGE_1; statusElement.innerHTML = '画像1(' + IMAGE_1 + ')';}}
-function checkStatusAlert(){const currentStatus = document.getElementById('statusInfo').inner HTML; alert('画像:　' + currentStatus);}
-
+function imgchange1(){document.getElementById('logo').src ='./image/20240712.jpg';}
+function imgback(){document.getElementById('logo').src ='./image/keiei_small.jpg';}
                             
                                      
