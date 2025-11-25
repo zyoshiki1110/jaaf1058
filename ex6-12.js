bfb4964999bb4b15264e684bb650addb24a30bd5
zyoshiki1110 <zm23441@josai.ac.jp>
2025-11-25 16:39:14 +0900

    Update ex6-12.js

--- ex6-12.js
+++ ex6-12.js
@@ -1,2 +1,2 @@
-function imgchange1(){document.getElementById('logo').src ='./image/bulbon.webp';}
-function imgback(){document.getElementById('logo').src ='./image/josai.jpeg';}
+function imgchange1(){document.getElementById('logo').src ='./image/bulbon.gif';}
+function imgback(){document.getElementById('logo').src ='./image/josai.png';}
