ed418918644da4056cbd65745377c5c4652ff177
zyoshiki1110 <zm23441@josai.ac.jp>
2025-12-02 10:36:34 +0900

    Update ex6-13.js

--- ex6-13.js
+++ ex6-13.js
@@ -1,1 +1,6 @@
-function imgback1(){document.getElementById('logo').src ='./image/josai.png.';}
+
+function imgback1(){const logoimage = document.getElementById('logo');
+                   logoimage.classList.add('changed');
+                   logoimage.src ='./image/josai.png.';}
+
+function imagback1(){const logoimage = document.getElementById('logo');}
