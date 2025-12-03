286ada8dc9e50963335da75be15bfbbdebde125e
zyoshiki1110 <zm23441@josai.ac.jp>
2025-12-03 09:56:06 +0900

    Fix image change functions for logo element

--- ex6-13.js
+++ ex6-13.js
@@ -1,1 +1,2 @@
-function imgback1(){document.getElementById('logo').src ='./image/josai.png';}
+function imgchange2(){document.getElementById('logo').src ='./image/josai.png' class="imagechange";}
+function imgback1(){document.getElementById('logo').src ='./image/josai.png' class="imagechanged";}
