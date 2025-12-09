d27cc07584ae0a75e0e59e1e16624a0f7c39aeff
zyoshiki1110 <zm23441@josai.ac.jp>
2025-12-09 10:22:27 +0900

    Update ex06-15.js

--- ex06-15.js
+++ ex06-15.js
@@ -1,3 +1,3 @@
 function hyoji(){var str = "一文字ずつ表示数。";
-                 var cnt = document.timer.moji.value.length;
+                 var cnt = document.timer.moji.value.length;}
                  
