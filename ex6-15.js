41a19f00f61c9373a9b0772b3f415e2ea886e2b9
zyoshiki1110 <zm23441@josai.ac.jp>
2025-12-16 09:42:27 +0900

    Update ex6-15.js

--- ex6-15.js
+++ ex6-15.js
@@ -1,3 +1,7 @@
 function hyouji(){
   var str = "一文字ずつ表示します。";
   var cnt = document.timer.moji.value.length;
+  if(cnt<11){
+    document.timer.moji.value = str.substr(0,cnt+1);}else{
+    document.timer.moji.value = "";}}
+function startfnc(){setlnterval("hyoji",1000);}
