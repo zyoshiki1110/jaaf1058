31dde6171505fba8c95bf78a056ea9dabf93324b
zyoshiki1110 <zm23441@josai.ac.jp>
2025-12-16 10:23:02 +0900

    Update ex6-17.js

--- ex6-17.js
+++ ex6-17.js
@@ -1,1 +1,12 @@
-
+function hyoji17(){
+//表示する文字
+var str = "一文字ずつ表示します。"
+//テキストボックスの文字数
+var cnt17 = document.timer17.moji17.value.length;
+//文字が全部表示されているか確認
+if(cnt17<11){
+//現在より１文字多く切り出して表示
+document.timer17.moji17.value = str17.substr(0,cnt17+1);}else{
+//全て表示されたら、空文字に戻す
+document.timer17.moji17.value="";}
+setTimeout("hyoji17()",1000);}
