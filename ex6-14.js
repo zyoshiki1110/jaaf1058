1c049ab8628b943bd156286993b54594e3a0b513
zyoshiki1110 <zm23441@josai.ac.jp>
2025-12-23 00:28:39 +0900

    Create ex6-14.js

---
+++ ex6-14.js
@@ -0,0 +1,6 @@
+function changeFace() {var area = document.getElementById('photo-area');
+                       area.innerHTML = '<img src="image/IMG_3598.jpeg" alt="変な顔の写真">';}
+
+
+function resetFace() {var area = document.getElementById('photo-area');
+                      area.innerHTML = '<img src="image/IMG_6513.jpeg" alt="真面目な顔の写真">';}
