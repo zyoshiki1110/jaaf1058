e29afec1643c48b2fe14e9526306f24f0a301e5d
zyoshiki1110 <zm23441@josai.ac.jp>
2025-12-22 21:41:53 +0900

    Update ex6-15.js

--- ex6-15.js
+++ ex6-15.js
@@ -1,7 +1,15 @@
-function hyouji(){
-  var str = "一文字ずつ表示します。";
-  var cnt = document.timer.moji.value.length;
-  if(cnt<11){
-    document.timer.moji.value = str.substr(0,cnt+1);}else{
-    document.timer.moji.value = "";}}
-function startfnc(){setInterval("hyoji()",1000);}
+function hyoji(){
+//表示する文字
+var str = "一文字ずつ表示します。";
+//テキストボックスの文字数
+var cnt = document.timer.moji.value.length;
+//文字が全部表示されてるか確認
+if(cnt < 11){
+//現在より１文字多く切り出して表示
+document.timer.moji.value = str.substr(0,cnt+1);}
+else{
+//全て表示されたら、空文字に戻す
+document.timer.moji.value = "";}}
+functionv startfnc(){
+//関数
+settInterval("hyoji()",1000);}
