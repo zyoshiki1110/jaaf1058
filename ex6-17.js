1778b61f23de9244a6ae12dd70315a274b779623
zyoshiki1110 <zm23441@josai.ac.jp>
2025-12-22 22:28:56 +0900

    Update ex6-17.js

--- ex6-17.js
+++ ex6-17.js
@@ -1,7 +1,6 @@
 function hyoji17(){
-var str17 = "一文字ずつ表示します。";
-var cnt17 = document.timer17.moji17.value.length;
-if(cnt < 11){
-document.timer17.moji17.value = str17.substr(0,cnt17+1);}else{
-document.timer17.moji17.value = "";}
-setTimeout("hyoji17()",1000);}
+  var str17 = "一文字ずつ表示します。";
+  var cnt17 = document.timer17.moji17.value.length;
+ if(cnt17 < 11){ document.timer17.moji17.value = str17.substr(0, cnt17 + 1); } 
+ else {document.timer17.moji17.value = ""; }
+  setTimeout("hyoji17()", 1000);}
