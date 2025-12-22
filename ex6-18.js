807dc5129d37efa9360eb8c4d96d81f49a2a29ca
zyoshiki1110 <zm23441@josai.ac.jp>
2025-12-22 22:49:12 +0900

    Create ex6-18.js

---
+++ ex6-18.js
@@ -0,0 +1,3 @@
+function changeImage() {document.getElementById("myImage").src = "image/keiei_small.jpg";
+                        setTimeout(backImage, 3000);}
+function backImage() {document.getElementById("myImage").src = "image/josai.png";}
