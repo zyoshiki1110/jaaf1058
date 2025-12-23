d3a3590a60e1b23ebfc7f306f0b96027a7392178
zyoshiki1110 <zm23441@josai.ac.jp>
2025-12-23 10:21:48 +0900

    Update ex7-5.js

--- ex7-5.js
+++ ex7-5.js
@@ -1,2 +1,2 @@
-function nazo() {if (document.getElementById('answer').value == '夢') {alert('あたり！');} else {alert('はずれ！');}
+function nazo() {if (document.getElementById('answer01').value == '夢') {alert('あたり！');} else {alert('はずれ！');}
 }
