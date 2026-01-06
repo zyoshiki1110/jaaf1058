4b0ada47cc6face6ac29c67c92135a45f0904d54
zyoshiki1110 <zm23441@josai.ac.jp>
2026-01-06 09:48:19 +0900

    Update input element ID in nazo77 function

--- ex7-7.js
+++ ex7-7.js
@@ -1,3 +1,3 @@
-function nazo77() {if (document.getElementById('answer').value == '夢') {
+    function nazo77() {if (document.getElementById('answer77').value == '夢') {
         alert('あたり!');} else {
         alert('はずれ!');}}
