f813ad8eaafb9d747b83755534dea47bbf54fce9
zyoshiki1110 <zm23441@josai.ac.jp>
2026-01-06 10:18:04 +0900

    Rename calcBMI function and update element IDs

--- ex7-10.js
+++ ex7-10.js
@@ -1,9 +1,9 @@
-function calcBMI() {var h_cm = document.getElementById('height_cm').value;
-                    var w_kg = document.getElementById('weight_kg').value;
+function calcBMI710() {var h_cm = document.getElementById('height_cm710').value;
+                    var w_kg = document.getElementById('weight_kg710').value;
                     var h_m = Number(h_cm) / 100;
                     var weight = Number(w_kg);
                     var bmi = weight / (h_m * h_m);
                     var resultText = "あなたのBMIは " + bmi.toFixed(2) + " です。";
                     var hantei = "";
 if (bmi < 18.5) {hantei = "判定：低体重";} else {if (bmi < 25) {hantei = "判定：普通";} else {hantei = "判定：肥満";}}
-document.getElementById('result').innerText = resultText + "\n" + hantei;}
+document.getElementById('result710').innerText = resultText + "\n" + hantei;}
