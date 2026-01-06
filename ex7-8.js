0960ee838a5562a51056e94a362c1371bafc3bed
zyoshiki1110 <zm23441@josai.ac.jp>
2026-01-06 09:54:06 +0900

    Fix function to check answer from answer78 element

--- ex7-8.js
+++ ex7-8.js
@@ -1,2 +1,2 @@
-function nazo77() {if (document.getElementById('answer77').value == '夢') {alert('あたり!');} 
+function nazo77() {if (document.getElementById('answer78').value == '夢') {alert('あたり!');} 
                    else {alert('はずれ!');}}
