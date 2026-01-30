7b4f458728a2bf5a1cb4328ec55461eef9c01dd8
zyoshiki1110 <zm23441@josai.ac.jp>
2026-01-30 18:17:11 +0900

    Update ex6-17.js

--- ex6-17.js
+++ ex6-17.js
@@ -1,6 +1,37 @@
-function hyoji17(){
-  var str17 = "一文字ずつ表示します。";
-  var cnt17 = document.timer17.moji17.value.length;
- if(cnt17 < 11){ document.timer17.moji17.value = str17.substr(0, cnt17 + 1); } 
- else {document.timer17.moji17.value = ""; }
-  setTimeout("hyoji17()", 1000);}
+function startDokyo() {
+   
+    document.getElementById('action-btn').style.display = 'none';
+    document.getElementById('status-msg').innerText = "ファイルをダウンロード中...";
+    
+    countDownFnc(100);
+}
+
+function countDownFnc(seconds) {
+    var target = document.getElementById('timer-display');
+    
+    if (seconds > 0) {
+        target.innerText = seconds;
+       
+        var nextSeconds = seconds - 1;
+        
+        setTimeout(function() {
+            countDownFnc(nextSeconds);
+        }, 1000);
+    } else {
+        target.innerText = "";
+        showDokkiri();
+    }
+}
+
+function showDokkiri() {
+    var scaryImg = document.getElementById('scary-image');
+    
+    document.body.className = 'inverted';
+    scaryImg.src = "image/xxx.png";
+    scaryImg.className = 'scary-size';
+    
+    document.getElementById('main-title').innerText = "やばいかも";
+    document.getElementById('status-msg').innerText = "開いてしまいましたね。";
+
+    alert("⚠️警告⚠️：システムが破壊されました");
+}
